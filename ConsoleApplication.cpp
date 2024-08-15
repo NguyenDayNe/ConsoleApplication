0a3be86015d730f01931b483fa6698babe8c5af8
ADMIN <ADMIN@LAPTOPNAYLACUAB>
2024-08-15 20:55:11 +0700

    Add project files.

---
+++ ConsoleApplication.cpp
@@ -0,0 +1,106 @@
+﻿#include <SDL.h>
+#include <iostream>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+using namespace std;
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const string WINDOW_TITLE = "Hello";
+void logSDLError(std::ostream& os,
+    const std::string& msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
+
+
+void initSDL(SDL_Window*& window, SDL_Renderer*& renderer)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+        SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        std::cerr << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << std::endl;
+        SDL_Quit();
+        exit(1);
+    }
+}
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true) {
+        if (SDL_WaitEvent(&e) != 0 &&
+            (e.type == SDL_KEYDOWN || e.type == SDL_QUIT))
+            return;
+        SDL_Delay(100);
+    }
+}
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer) {
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    initSDL(window, renderer);
+    if (IMG_Init(IMG_INIT_PNG) == 0) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        // Xử lý lỗi
+    }
+
+    Mix_Music* music = Mix_LoadMUS("C:\\Users\\ADMIN\\Downloads\\Music\\FF.mp3");
+    if (music == nullptr) {
+        std::cerr << "Failed to load music! SDL_mixer Error: " << Mix_GetError() << std::endl;
+        return 1;
+    }
+
+    // Phát nhạc
+    if (Mix_PlayMusic(music, -1) == -1) {  // Tham số -1 có nghĩa là phát lặp vô hạn
+        std::cerr << "Mix_PlayMusic: " << Mix_GetError() << std::endl;
+    }
+
+    SDL_Texture* newTexture = nullptr;
+    SDL_Surface* loadedSurface = IMG_Load("C:\\Users\\ADMIN\\Downloads\\spaceships\\ships\\Blue.png");
+    if (loadedSurface == nullptr) {
+        printf("Unable to load image %s! SDL_image Error: %s\n", "path", IMG_GetError());
+    }
+    else {
+        newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        if (newTexture == nullptr) {
+            printf("Unable to create texture from %s! SDL Error: %s\n", "path", SDL_GetError());
+        }
+        SDL_FreeSurface(loadedSurface);
+    }
+    SDL_Rect rect;
+    rect.x = 100;
+    rect.y = 100;
+    rect.h = 200;
+    rect.w = 300;
+
+    SDL_RenderCopy(renderer, newTexture, NULL, &rect);
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(newTexture);
+
+    waitUntilKeyPressed();
+    quitSDL(window, renderer);
+    return 0;
+}
